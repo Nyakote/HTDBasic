e89f976154397d66dd5f75367943b89434b89fff
F1rstv5yer <f1rstv5yer@gmail.com>
2025-02-11 01:08:44 +0200

    add working LateranoGun with LateranoRocket

---
+++ Content/Items/Range/LateranoGun.cs
@@ -0,0 +1,67 @@
+﻿using HTDBasic.Content.Projectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HTDBasic.Content.Items.Range
+{
+    public class LateranoGun : ModItem
+    {
+        public override void SetDefaults()
+        {
+
+            Item.width = 39;
+            Item.height = 13;
+            Item.scale = 2f;
+            Item.rare = ItemRarityID.Green;
+
+            Item.useTime = 50; 
+            Item.useAnimation = 50; 
+            Item.useStyle = ItemUseStyleID.Shoot; 
+            Item.autoReuse = true;
+
+            Item.UseSound = new SoundStyle($"{nameof(HTDBasic)}/Content/Sounds/lateranoweapon_projectile_start")
+            {
+                Volume = 0.8f,
+                PitchVariance = 0.2f,
+                MaxInstances = 3
+            };
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 100; 
+            Item.knockBack = 5f; 
+            Item.noMelee = true;
+
+            Item.shoot = ModContent.ProjectileType<LateranoRocket>(); 
+            Item.shootSpeed = 20f;
+            Item.useAmmo = AmmoID.Rocket;
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+/*        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<ExampleItem>()
+                .AddTile<Tiles.Furniture.ExampleWorkbench>()
+                .Register();
+        }*/
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-12f, -2f);
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+/*            // Every projectile shot from this gun has a 1/3 chance of being an ExampleInstancedProjectile
+            if (Main.rand.NextBool(3))
+            {
+                type = ModContent.ProjectileType<ExampleInstancedProjectile>();
+            }*/
+        }
+
+    }
+}
---
+++ Content/Projectiles/LateranoRocket.cs
@@ -0,0 +1,121 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+
+namespace HTDBasic.Content.Projectiles
+{
+    public class LateranoRocket : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 14;
+            Projectile.height = 14;
+            Projectile.aiStyle = 0;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 300;
+            Projectile.light = 0.5f;
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = false;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            Projectile.ai[0] += 1f;
+
+            if (Projectile.ai[0] < 2f)
+            {
+                Projectile.velocity.Y *= 0.98f;
+            }
+            else
+            {
+                Projectile.ai[0] = 2f;
+
+                Projectile.velocity.Y += 0.3f;
+
+                if (Projectile.velocity.Y > 20f)
+                {
+                    Projectile.velocity.Y = 20f;
+                }
+            }
+
+            if (Main.rand.NextBool(2))
+            {
+                Vector2 dustPosition = Projectile.Center - Projectile.velocity * 0.5f;
+            
+                Dust dust = Dust.NewDustDirect(dustPosition, 1, 1, DustID.Torch,-Projectile.velocity.X * 0.3f, -Projectile.velocity.Y * 0.3f, 100, default, 2f);
+                dust.noGravity = true;
+                dust.color = new Color(255, 105, 0);
+                dust.scale *= 1.5f;
+                dust.fadeIn = 0.3f;
+            }
+
+            Lighting.AddLight(Projectile.Center, 1f, 0.3f, 0f);
+
+            if (Main.rand.NextBool(2))
+            {
+                Vector2 dustPosition = Projectile.Center - Projectile.velocity * 0.5f;
+                Dust dust = Dust.NewDustDirect(dustPosition, 1, 1, DustID.Torch, -Projectile.velocity.X * 0.3f, -Projectile.velocity.Y * 0.3f, 100, default, 1.5f);
+                dust.noGravity = true;
+            }
+        }
+
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            Projectile.Kill();
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            //не реализованный массовый урон по противникам
+
+            /*float explosionRadius = 150f;
+
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC npc = Main.npc[i];
+                if (npc.active && !npc.friendly && Vector2.Distance(Projectile.Center, npc.Center) < explosionRadius)
+                {
+                    npc.StrikeNPC(Projectile.damage, 0, 0, crit: false);
+                }
+            }*/
+
+            for (int i = 0; i < 20; i++)
+            {
+                int dustType = DustID.Torch;
+                Vector2 position = Projectile.Center + new Vector2(Main.rand.Next(-150, 130), Main.rand.Next(-150, 130)); // Более широкий радиус
+                Dust dust = Dust.NewDustDirect(position, 1, 1, dustType, 0f, 0f, 0, default, 2f);
+                dust.noGravity = true;
+                dust.scale *= 2f;
+                dust.fadeIn = 0.5f;
+            }
+
+            Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, new Vector2(Main.rand.Next(-3, 4), Main.rand.Next(-3, 4)), Main.rand.Next(61, 64), 1f);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Vector2 position = Projectile.Center + new Vector2(Main.rand.NextFloat(-20f, 20f), Main.rand.NextFloat(-20f, 20f));
+                Dust dust = Dust.NewDustDirect(position, 1, 1, DustID.Torch, 0f, 0f, 0, default, 3f);
+                dust.noGravity = true;
+                dust.fadeIn = 0.3f;
+                dust.scale *= 2f;
+            }
+            /*for (int i = 0; i < 20; i++)
+            {
+                int dustIndex = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.OrangeTorch, Main.rand.NextFloat(-3, 3), Main.rand.NextFloat(-3, 3), 150, default, 1.5f);
+                Main.dust[dustIndex].velocity *= 2;
+                Main.dust[dustIndex].noGravity = true;
+            }*/
+
+            Terraria.Audio.SoundEngine.PlaySound(new SoundStyle($"{nameof(HTDBasic)}/Content/Sounds/lateranoweapon_projectile_reach"), Projectile.position);
+        }
+    }
+}
